cb9f7fe5b94888f4314c77565819c1b25271076b
Leff-isonfire <Jason@JasonPandeMacBook-Pro.local>
2015-08-03 14:53:09 +0800

    Initial Commit

---
+++ Mr_K_DemoTests/Mr_K_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mr_K_DemoTests.swift
+//  Mr_K_DemoTests
+//
+//  Created by Leff on 15/8/3.
+//  Copyright (c) 2015年 LeffPan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Mr_K_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
